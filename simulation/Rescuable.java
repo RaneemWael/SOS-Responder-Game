4199ab3c6dfa51af9354041816084fe39242da51
Raneem Wael <raneemwael99@gmail.com>
2022-03-18 19:17:54 +0200

    Delete simulation directory

--- simulation/Rescuable.java
+++
@@ -1,9 +0,0 @@
-package simulation;
-
-import model.disasters.Disaster;
-
-public interface Rescuable {
-public void struckBy(Disaster d);
-public Address getLocation();
-public Disaster getDisaster();
-}
